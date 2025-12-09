cb96777d48dfbb702373d0d9ec5c3638a90a4d98
Cyber-701 <abdugofurovbehruzbek701@gmail.com>
2025-12-09 16:27:29 +0500

    create Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,17 @@
 # In api/serializers.py
 from rest_framework.serializers import ModelSerializer
 from posts.models import Post
+from django.contrib.auth.models import User
 
-class PostSerializer(ModelSerializer):  # Changed from PostSerializers to PostSerializer
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+class PostSerializer(ModelSerializer):
+    author = UserSerializer()  # Changed from PostSerializers to PostSerializer
     class Meta:
         model = Post
         fields = ["id", "author", "title", "body", "created", "updated"]
+        depth = 1
